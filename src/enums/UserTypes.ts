931d4d2be7972855acb3c948d129d6d992a6b2a4
Joshlanuevo <lanuevo68@gmail.com>
2025-04-03 17:25:54 +0800

    add user type enum

---
+++ src/enums/UserTypes.ts
@@ -0,0 +1,10 @@
+enum UserTypes {
+    SUPERADMIN = "superadmin",
+    ADMIN = "admin",
+    MASTERAGENT = "masteragent",
+    AGENT = "agent",
+    SUBAGENT = "subagent",
+    WHITELABEL = "whitelabel",
+    ACCOUNTING = "accounting",
+    MERCHANT = "merchant",
+}
